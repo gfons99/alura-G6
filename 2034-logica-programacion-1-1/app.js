3309ed03dd11fca70edd072982170b1d78a5b1cf
gfons99 <igadu_99@outlook.com>
2024-01-23 16:58:28 -0600

    curso 1-1 completo

---
+++ 2034-logica-programacion-1-1/app.js
@@ -0,0 +1,9 @@
+// alert('Hola Mundo');
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Indíqueme un número, por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+     alert("¡Adivinaste el número!")
+}
